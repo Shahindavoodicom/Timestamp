df776788d5b6f5d937cd3fbc3d26dbb5f6290cac
shahin davoodi <shahindavoodi1379@gmail.com>
2024-02-08 16:45:38 +0330

    url shourt

--- index.js
+++ index.js
@@ -4,23 +4,33 @@ const port = 5000;
 const cors = require('cors');
 app.use(express.json());
 app.use(cors());
-app.get('/api/:date?', (req, res) => {
-    let date = req.params.date;
-    if(!isNaN(date)) {
-        date = parseInt(date);
-    }
-    if (!date) {
-        res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
-    }
-    const dateObject = new Date(date);
-    if (dateObject.toString() === 'Invalid Date') {
-        res.json({ error: 'Invalid Date' });
-    }else {
-        res.json({ unix: dateObject.getTime(), utc: dateObject.toUTCString() });
+//Waiting:You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
+// Waiting:When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+// Waiting:If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+
+let urlList = [];
+let shortUrl = 0;
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body;
+  if (url.match(/^(http|https):\/\/[^ "]+$/)) {
+    shortUrl++;
+    urlList.push({ original_url: url, short_url: shortUrl });
+    res.json({ original_url: url, short_url: shortUrl });
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+    const { short_url } = req.params;
+    const url = urlList.find((url) => url.short_url === +short_url);
+    if (url === undefined) {
+        res.json({ error: 'No short URL found for the given input' });
+    } else {
+        res.redirect(url.original_url);
     }
 }
 );
-
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
