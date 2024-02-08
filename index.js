ce207d506406488d506d34d63172f353d569c688
shahin davoodi <shahindavoodi1379@gmail.com>
2024-02-08 16:38:57 +0330

    timestamp cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const cors = require('cors');
 app.use(express.json());
-
+app.use(cors());
 app.get('/api/:date?', (req, res) => {
     let date = req.params.date;
     if(!isNaN(date)) {
