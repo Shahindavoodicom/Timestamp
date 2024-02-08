00286263f4557f33e06bc0461db362ea8389bc73
shahin davoodi <shahindavoodi1379@gmail.com>
2024-02-08 16:32:54 +0330

    secend project

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-
+let cors = require('cors');
 app.use(express.json());
+app.use(cors());
+//Waiting:A request to /api/whoami should return a JSON object with your IP address in the ipaddress key.
+// Waiting:A request to /api/whoami should return a JSON object with your preferred language in the language key.
+// Waiting:A request to /api/whoami should return a JSON object with your software in the software key.
 
-app.get('/api/:date?', (req, res) => {
-    let date = req.params.date;
-    if(!isNaN(date)) {
-        date = parseInt(date);
-    }
-    const dateObject = new Date(date);
-    if (dateObject.toString() === 'Invalid Date') {
-        res.json({ error: 'Invalid Date' });
-    }else {
-        res.json({ unix: dateObject.getTime(), utc: dateObject.toUTCString() });
-    }
-}
-);
-
+app.get('/api/whoami', (req, res) => {
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent'],
+    });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
