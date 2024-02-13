408ba09a32e857559b4f8fcb234c7c843d9ccca4
shahin davoodi <shahindavoodi1379@gmail.com>
2024-02-13 12:11:44 +0330

    exercises project codes

--- index.js
+++ index.js
@@ -3,22 +3,52 @@ const app = express();
 const port = 5000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-// You should provide your own project, not the example URL.
-// You can submit a form that includes a file upload.
-// The form file input field has the name attribute set to upfile.
-// When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+
+let users = [];
+let exercises = [];
+let logs = [];
+
+app.post('/api/users', (req, res) => {
+    const { username } = req.body;
+    const id = users.length + 1;
+    users.push({ username, id });
+    res.json({ username, id });
 });
-app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
-    const { originalname, mimetype, size } = req.file;
-    res.json({ name: originalname, type: mimetype, size });
+
+app.get('/api/users', (req, res) => {
+    res.json(users);
 });
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+    const { _id } = req.params;
+    const { description, duration, date } = req.body;
+    const user = users.find((user) => user.id === parseInt(_id));
+    const log = { description, duration, date };
+    const exercise = { username: user.username, _id, log };
+    exercises.push(exercise);
+    res.json(exercise);
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+    const { _id } = req.params;
+    const user = users.find((user) => user.id === parseInt(_id));
+    const log = exercises.filter((exercise) => exercise._id === parseInt(_id));
+    const count = log.length;
+    res.json({ username: user.username, _id, count, log });
+});
+
+app.get('/api/exercises', (req, res) => {
+    res.json(exercises);
+})
+
+app.get('/api/logs', (req, res) => {
+    res.json(logs);
+})
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
