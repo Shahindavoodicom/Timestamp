284f85a870f6f1a9252ae9a8e1af330fcbc62008
shahin davoodi <shahindavoodi1379@gmail.com>
2024-02-13 12:05:28 +0330

    file project codes 3

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const multer = require('multer');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-
+// You should provide your own project, not the example URL.
+// You can submit a form that includes a file upload.
+// The form file input field has the name attribute set to upfile.
+// When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
 app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
     const { originalname, mimetype, size } = req.file;
     res.json({ name: originalname, type: mimetype, size });
