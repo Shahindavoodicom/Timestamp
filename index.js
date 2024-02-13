6b5eb7d6aaf75b729fbb7ef8040d45b35a352186
shahin davoodi <shahindavoodi1379@gmail.com>
2024-02-13 11:49:20 +0330

    file project codes

--- index.js
+++ index.js
@@ -8,36 +8,21 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-//Waiting:You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties. Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
-// Waiting:When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
-// Waiting:If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain { error: 'invalid url' }
+// Waiting:You can submit a form that includes a file upload.
+// Waiting:The form file input field has the name attribute set to upfile.
+// Waiting:When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
 
-let urlList = [];
-let shortUrl = 0;
-app.post('/api/shorturl',  (req, res) => {
-    const url = req.body.url;
-    console.log(url)
-  if (!url) {
-    res.json({ error: 'No url provided' });
-  } else if (url.match(/^(http|https):\/\/[^ "]+$/)) {
-    shortUrl++;
-    urlList.push({ original_url: url, short_url: shortUrl });
-    res.json({ original_url: url, short_url: shortUrl });
-  } else {
-    res.json({ error: 'invalid url' });
-  }
-});;
-
-app.get('/api/shorturl/:short_url', (req, res) => {
-    const { short_url } = req.params;
-    const url = urlList.find((url) => url.short_url === +short_url);
-    if (url === undefined) {
-        res.json({ error: 'No short URL found for the given input' });
+app.post('/api/fileanalyse', (req, res) => {
+    if (req.files) {
+        const file = req.files.upfile;
+        const name = file.name;
+        const type = file.mimetype;
+        const size = file.size;
+        res.json({ name, type, size });
     } else {
-        res.redirect(url.original_url);
+        res.json({ error: 'No file uploaded' });
     }
-}
-);
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
